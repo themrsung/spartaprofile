1ece3277ef069878c55d681cf353b296efaf6015
MJ Sung <themrsung@gmail.com>
2022-11-03 08:53:42 +0900

    main.html

--- app.py
+++ app.py
@@ -190,6 +190,10 @@ def seongchange():
 def leo():
     return render_template('leo.html')
 
+@app.route('/main', methods=['GET', 'POST'])
+def main():
+    return render_template('main.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
