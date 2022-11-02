7b047ccc4b972ff97fd6e3477492ec89885020c9
MJ Sung <themrsung@gmail.com>
2022-11-02 11:00:57 +0900

    fixed link

--- app.py
+++ app.py
@@ -119,9 +119,6 @@ def mjsunggetrep():
 def dain():
     return render_template('pro.html')
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
-
 @app.route('/leetaehyun', methods=['GET', 'POST'])
 def leetaehyun():
     return render_template('LeeTaeHyun.html')
