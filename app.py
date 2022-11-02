a581d67aadd0812f426544b448785140db192f80
MJ Sung <themrsung@gmail.com>
2022-11-02 10:48:22 +0900

    Added taehyun sublink

--- app.py
+++ app.py
@@ -121,3 +121,10 @@ def dain():
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
+@app.route('/leetaehyun', methods=['GET', 'POST'])
+def leetaehyun():
+    return render_template('LeeTaeHyun.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
